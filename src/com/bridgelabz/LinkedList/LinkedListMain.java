7e7f639e67d234821ba0f26f5953130ea9d5c36c
PremKumbhar <kumbharprem7738@gmail.com>
2022-08-28 11:31:51 +0530

    [Prem]:adding create linked list by appending 30 and 70 to 56 file

--- src/com/bridgelabz/LinkedList/LinkedListMain.java
+++ src/com/bridgelabz/LinkedList/LinkedListMain.java
@@ -7,13 +7,17 @@ public class LinkedListMain {
         System.out.println("Welcome to linkedList DataStructure Program");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 to add the data at the start"); // UC 2
-        System.out.println("Enter 2 to Exit");
+        System.out.println("Enter 2 to add the data at the End"); // UC 3
+        System.out.println("Enter 3 to Exit");
         switch (scanner.nextInt()) {
 
             case 1:
                 Operations.addDataAtStart();
                 break;
             case 2:
+                Operations.addDataAtEnd();
+                break;
+            case 3:
             	System.out.println("Exit");
             	break;
             default:
